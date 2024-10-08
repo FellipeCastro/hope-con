522eb3fc6cff0443335b12ed356aea0fcc5d37e0
Fellipe Castro <fellipecastro27d@gmail.com>
2024-10-08 18:06:34 -0300

    Home iniciada

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hope Con'2024</title>
 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
     <link rel="stylesheet" href="style.css">
+
 
     <!-- GOOGLE FONTS - OPEN SANS/CHERRY BOMB -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,16 +16,27 @@
     <link href="https://fonts.googleapis.com/css2?family=Cherry+Bomb+One&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 </head>
 <body>
+    <!-- HEADER  -->
     <header class="header">
         <a href="#" class="logo">
-            <img src="img/logo.png" alt="logo">
+            <img src="img/hope-con-logo.png" alt="logo">
         </a>
 
         <nav>
-            <a href="#">Home</a>
+            <a href="#home">Home</a>
             <a href="#">Sobre</a>
             <a href="#">Preletores</a>
         </nav>
     </header>
+
+    <!-- HOME -->
+     <section class="home" id="home">
+        <div class="text-container-home">
+            <img src="img/hope-con-logo.png" alt="logo">
+            <p>10 de Novembro</p>
+        </div>
+
+        <a href="#">Garanta seu ingresso</a>
+     </section>
 </body>
 </html>
